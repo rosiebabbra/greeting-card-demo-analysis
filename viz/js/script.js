ccd68a8dafbc2e443501cd99ad35d898b10dd831
Rosie Babbra <rosiebabbra@gmail.com>
2019-07-01 22:19:04 -0700

    Add files via upload
    appended labels to svgs, prohibited user selection of more than two categories

--- viz/js/script.js
+++ viz/js/script.js
@@ -1,4 +1,3 @@
-// array to hold image links
 cardImages = {}
 
 // currently, the data is re-read everytime the chart is updated
@@ -8,7 +7,7 @@ function readDataAndDrawCards(filter){
   cardImages[filter] = []
 
   // import data from csv, push s3_link to array if the category is = filter
-  d3.csv("../data/hallmark_card_rgb_codes_sorted.csv", function(data){
+  d3.csv("../data/hallmark_card_sorted_by_hue.csv", function(data){
     for (i=0;i<data.length;i++){
       if (data[i].category == filter){
       cardImages[filter].push(data[i].s3_link);
@@ -27,21 +26,25 @@ function drawCards(idName){
   var body = d3.select("body");
 
   // equal width and height so svg is a square
-  var width = document.getElementById('chart').clientHeight;
+  var width = document.getElementById('chart').clientHeight * .75;
   var height = document.getElementById('chart').clientHeight;
 
   var svg = d3.select("#chart").append("svg")
           .attr("width", width)
           .attr("height", height)
           .attr("id",idName);
+
+  var label = svg.append('text')
+          .text(idName)
+          .attr('x', 5)
+          .attr('y', 20)
+          .attr('font-size', '18px');
   
   var defs = svg.append('svg:defs')
 
   var config = {
       "card_size" : width / Math.ceil(Math.sqrt(numberOfCards)) // sqrt (rounded up) of numberOfCards is # of rows and columns for the grid
   }
-
-  var body = d3.select("body");
 
   var y = 0; // y placement on svg for rects
   var x = 0; // x placement on svg for rects
@@ -76,69 +79,64 @@ function drawCards(idName){
     svg.append("rect")
           .attr("class", "card")
           .attr("x", x*config.card_size)
-          .attr("y", y*config.card_size)
+          .attr("y", y*config.card_size + config.card_size)
           .attr("width", config.card_size)
           .attr("height",config.card_size)
           .attr("id",i)
           .style("fill", "url(#" + idName+i.toString() +")") // calls a defs by id to determine image
           .on("mouseover", function(){ 
-              div.html("<img class='onHoverFirst' style='height: " + config.card_size * 1 + "px; width: " + config.card_size * 1 + "px;' src=" + cardImages[idName][d3.select(this).attr('id')] + ">")
+
+              // var style = 'height: " + config.card_size + "px; width: " + config.card_size + "px;'
+              var style_min = "'height: " + config.card_size + "px; width: " + config.card_size + "px;'"
+              var style_max = "'height: " + config.card_size * 4 + "px; width: " + config.card_size * 4 + "px;'"
+              var src = cardImages[idName][d3.select(this).attr('id')]
+
+              div.html("<img class='onHoverFirst' style='height: ${style_min}' src=${src}>")
                     .style("left", (d3.event.pageX) + "px")		
                     .style("top", (d3.event.pageY) + "px")
               div.transition()
                     .duration(500)		
                     .style("opacity", 1)
-              div.html("<img class='onHoverSecond' style='height: " + config.card_size * 4 + "px; width: " + config.card_size * 4 + "px;' src=" + cardImages[idName][d3.select(this).attr('id')] + ">")
+              div.html("<img class='onHoverSecond' style="+style_max+" src="+src+">")
                     .style("left", (d3.event.pageX) + "px")		
                     .style("top", (d3.event.pageY) + "px")
-              img.onHoverFirst.transition()
-                .style("height","100px")
-                .style("width","200px")
             })
-        .on("mouseout", function() {		
-          div.transition()		
-              .duration(500)		
-              .style("opacity", 0);	
-          });
+            .on("mouseout", function() {		
+              div.transition()		
+                  .style("opacity", 0);	
+              });
+
       
     // increment x to move to next column
     x = x + 1;
     counter = counter + 1
   }
 
+} 
+
+function limitCheckboxSelection() {
+
+  var checkboxes = document.getElementsByTagName('input');
+  var numChecked = 0;
+  var count;
+
+  for(count=0; count<checkboxes.length; count++){
+    if(checkboxes[count].checked==true){
+      numChecked = numChecked + 1;
+    }
+  }
+
+  if(numChecked>=3){
+
+    document.getElementById('invalid').innerHTML = 'Please select a maximum of two categories.'
+    document.getElementById('invalid').style.color = '#d00'
+
+    return false;
+
+  }
+
 } // closes drawCards() function
 
-
-// currently, the data is re-read everytime the chart is updated
-// need to figure out how to read the data once and only once, to make more efficient
-// var filter = document.getElementById('group-selection').value;
-
-// readDataAndDrawCards('Girl'); 
-
-// dropdown - every time a new group is selected, remove the current svg and defs and call readDataAndDrawCards()
-// d3.select("#group-selection").on("change", function() {
-//   d3.select("svg").remove()
-//   d3.select("defs").remove()
-//   var selectedOption = d3.select(this).property("value")
-//   readDataAndDrawCards(selectedOption)
-// })
-// every time a new group is selected, remove the current svg and defs and call readDataAndDrawCards()
-
-// if(d3.select("#girlCheckbox").property("checked")){
-//   readDataAndDrawCards('Girl');
-// } else {
-//   d3.select("svg").remove();
-//   d3.select("defs").remove();
-// }
-
-// d3.selectAll("#girlCheckbox").on("change", function() {
-//   var selected = this.value;
-//   opacity = this.checked ? 1 : 0;
-
-// svg.selectAll("svg")
-//   .filter(function(d) {return selected == d.category;})
-//   .style("opacity", opacity);
-//   });
 
 // when a checkbox value changes, add group svg if being checked or delete group svg if being unchecked
 d3.selectAll("input").on("change", function() {
@@ -158,10 +156,8 @@ d3.selectAll("input").on("change", function() {
     }
   }
 
-  console.log(numChecked);
-
   // original width and height of svgs
-  var width = document.getElementById('chart').clientHeight;
+  var width = document.getElementById('chart').clientHeight * .75;
   var height = document.getElementById('chart').clientHeight;
 
   // for every existing svg, resize depending of number of checked boxes - THIS ISN'T WORKING :(
@@ -173,26 +169,6 @@ d3.selectAll("input").on("change", function() {
   }
 
 })
-
-// examples old functions
-//d3.select("#girlCheckbox").on("change", function() {
-  //if (this.checked == false){
-    //document.getElementById('Girl').remove()
- // }
-  //else if (this.checked == true){
-    //readDataAndDrawCards('Girl')
- // }
-//})
-
-//d3.select("#boyCheckbox").on("change", function() {
-  //if (this.checked == false){
-   // document.getElementById('Boy').remove()
-  //}  
-  //else if (this.checked == true){
-    //readDataAndDrawCards('Boy')
-  //}
-//})
-
 
 
 // preload the page with Girl cards
