cb932b4b91c8462ff57f7e46ea7979121c339ef5
Oliver Gladfelter <ogladfelter@gmail.com>
2019-08-28 17:05:13 -0500

    Changes up to 8/28/19

--- viz/js/script.js
+++ viz/js/script.js
@@ -1,20 +1,28 @@
+function IsMobileCard()
+  {
+  var check =  false;
+
+  (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+
+  return check;   
+  }
+
 function readData(){ // reads csv, save all to a dictionary for later use
 
   // import data from csv, push s3_link to array if the category is = filter
-  d3.csv("../data/hallmark_card_with_rgb.csv", function(data){
+  d3.csv("./final_hallmark_card_data.csv", function(data){
     for (i=0;i<data.length;i++){
-      if (typeof cardImages[data[i].category] == "undefined"){ // first time seeing this category
-        cardImages[data[i].category] = [] // intialize the dictionary key and pair with an empty list
+      if (typeof cardImages[data[i].category] == "undefined"){
+        cardImages[data[i].category] = []
       }
       else { // category is already in dictionary
-        cardImages[data[i].category].push(data[i].s3_link); // add card link
+        cardImages[data[i].category].push(data[i].s3_resized_link); // add card link
       }
     }
 
   // preload svg1 with Girl cards
-  drawCardsLeft("Girl");
-  // preload svg2 with Boy cards
-  drawCardsRight("Boy");
+  drawCards("Girl");
+
   })
 
 }
@@ -22,7 +30,7 @@ function readData(){ // reads csv, save all to a dictionary for later use
 function readHueData(){ // reads csv, save all to a dictionary for later use
 
   // import data from csv, push s3_link to array if the category is = filter
-  d3.csv("../data/hallmark_card_with_rgb.csv", function(data){
+  d3.csv("./final_hallmark_card_data.csv", function(data){
     for (i=0;i<data.length;i++){
       if (typeof cardHues[data[i].category] == "undefined"){ // first time seeing this category
           cardHues[data[i].category] = [] // intialize the dictionary key and pair with an empty list
@@ -38,188 +46,49 @@ function readHueData(){ // reads csv, save all to a dictionary for later use
 
 }
 
-var width = document.getElementById('chart').clientHeight * .75; // testing smaller svg size; height currently not adjusted as cards become truncated
-var height = document.getElementById('chart').clientHeight;
-
-var svg1 = d3.select("#chart").append("svg")
-.attr("width", width)
-.attr("height", height)
-.attr("id", "svg1");
-
-var svg2 = d3.select("#chart").append("svg")
-.attr("width", width)
-.attr("height", height)
-.attr("id", "svg2");
-
-function drawCardsLeft(idName){ // activated every time selector is changed; draws new cards on svg1 for given 'idName' group
+// activated every time selector is changed; draws new cards for given 'idName' group
+function drawCards(idName){ 
 
   var numberOfCards = cardImages[idName].length;
 
-  var width = document.getElementById('chart').clientHeight * .75; // testing smaller svg size; height currently not adjusted as cards become truncated
-  var height = document.getElementById('chart').clientHeight;
+  // The way the grid format is being created does not allow
+  // obtaining of the each "squares" within the div, which means
+  // there is no identifier which to select each square and add an image
+  // absent of the for loop 
+  var grid = document.getElementById('galleryDiv');
+  grid.setAttribute('style','grid-template-columns: repeat('+Math.sqrt(numberOfCards)+', 2.5vw)')
+  grid.setAttribute('style','grid-template-rows: repeat('+Math.sqrt(numberOfCards)+', 2.5vw)');
 
-  var config = {
-    "card_size" : width / Math.ceil(Math.sqrt(numberOfCards)) // sqrt (rounded up) of numberOfCards is # of rows and columns for the grid
-  }
-
-  var defs = svg1.append('svg:defs');
-  
-  var y = 0; // y placement on svg for rects
-  var x = 0; // x placement on svg for rects
-  var counter = 0;
-
+  // //Start card population
   for (i=1;i<numberOfCards;i++) {
-
-    // once we've added enough cards to fill the row, increment y (to start the next row) and reset x
-    if (counter > Math.ceil(Math.sqrt(numberOfCards)-1)){
-      counter = 0;
-      y = y + 1; // move down a row
-      x = 0; // move back to the beginning column
-    }
-
-    // defs for images
-    defs.append("svg:pattern")
-    .attr("id", idName+i) // this id is what rect.style("fill") uses to pull in the right image
-    .attr("width", config.card_size)
-    .attr("height", config.card_size)
-    .attr("patternUnits", "userSpaceOnUse")
-    .append("svg:image")
-    .attr("xlink:href", cardImages[idName][i])
-    .attr("width", config.card_size)
-    .attr("height", config.card_size)
-    .attr("x", 0)
-    .attr("y", 0);
-
-    var div = d3.select("body").append("div")
-      .attr("class", "tooltip");
-
-    // add a rect, which will be filled with a card image
-    svg1.append("rect")
-          .attr("class", "card")
-          .attr("x", x*config.card_size)
-          .attr("y", y*config.card_size) 
-          .attr("width", config.card_size)
-          .attr("height",config.card_size)
-          .attr("id",i)
-          .style("fill", "url(#" + idName+i.toString() +")") // calls a defs by id to determine image
-          .on("mouseover", function(){ 
-
-              var style_min = "'height: " + config.card_size + "px; width: " + config.card_size + "px;'"
-              var style_max = "'height: " + height / 4 + "px; width: " + width / 4 + "px;'"
-              var src = cardImages[idName][d3.select(this).attr('id')]
-
-              div.html("<img class='onHoverFirst' style='height: "+style_min+"' src="+src+">")
-                    .style("left", (d3.event.pageX) + "px")		
-                    .style("top", (d3.event.pageY) + "px")
-              div.transition()
-                    .duration(500)		
-                    .style("opacity", 1)
-              div.html("<img class='onHoverSecond' style="+style_max+" src="+src+">")
-                    .style("left", (d3.event.pageX) + "px")		
-                    .style("top", (d3.event.pageY) + "px")
-            })
-            .on("mouseout", function() {		
-              div.transition()		
-                  .style("opacity", 0);	
-              });
-
-      
-    // increment x to move to next column
-    x = x + 1;
-    counter = counter + 1
+    var img = document.createElement('img');
+    img.src = cardImages[idName][i]
+    img.classList.add("gallery__img")
+    grid.appendChild(img)
   }
 
 } 
 
-function drawCardsRight(idName){ // activated every time selector is changed; draws new cards on svg2 for given 'idName' group
-
-  var numberOfCards = cardImages[idName].length;
-
-  var width = document.getElementById('chart').clientHeight * .75; // testing smaller svg size; height currently not adjusted as cards become truncated
-  var height = document.getElementById('chart').clientHeight;
-
-  var config = {
-    "card_size" : width / Math.ceil(Math.sqrt(numberOfCards)) // sqrt (rounded up) of numberOfCards is # of rows and columns for the grid
-  }
-
-  var defs = svg2.append('svg:defs');
-  
-  var y = 0; // y placement on svg for rects
-  var x = 0; // x placement on svg for rects
-  var counter = 0;
-
-  for (i=1;i<numberOfCards;i++) {
-
-    // once we've added enough cards to fill the row, increment y (to start the next row) and reset x
-    if (counter > Math.ceil(Math.sqrt(numberOfCards)-1)){
-      counter = 0;
-      y = y + 1; // move down a row
-      x = 0; // move back to the beginning column
-    }
-
-    // defs for images
-    defs.append("svg:pattern")
-    .attr("id", idName+i) // this id is what rect.style("fill") uses to pull in the right image
-    .attr("width", config.card_size)
-    .attr("height", config.card_size)
-    .attr("patternUnits", "userSpaceOnUse")
-    .append("svg:image")
-    .attr("xlink:href", cardImages[idName][i])
-    .attr("width", config.card_size)
-    .attr("height", config.card_size)
-    .attr("x", 0)
-    .attr("y", 0);
-
-    var div = d3.select("body").append("div")
-      .attr("class", "tooltip");
-
-    // add a rect, which will be filled with a card image
-    svg2.append("rect")
-          .attr("class", "card")
-          .attr("x", x*config.card_size)
-          .attr("y", y*config.card_size) 
-          .attr("width", config.card_size)
-          .attr("height",config.card_size)
-          .attr("id",i)
-          .style("fill", "url(#" + idName+i.toString() +")") // calls a defs by id to determine image
-          .on("mouseover", function(){ 
-
-              var style_min = "'height: " + config.card_size + "px; width: " + config.card_size + "px;'"
-              var style_max = "'height: " + height / 4 + "px; width: " + width / 4 + "px;'"
-              var src = cardImages[idName][d3.select(this).attr('id')]
-
-              div.html("<img class='onHoverFirst' style='height: "+style_min+"' src="+src+">")
-                    .style("left", (d3.event.pageX) + "px")		
-                    .style("top", (d3.event.pageY) + "px")
-              div.transition()
-                    .duration(500)		
-                    .style("opacity", 1)
-              div.html("<img class='onHoverSecond' style="+style_max+" src="+src+">")
-                    .style("left", (d3.event.pageX) + "px")		
-                    .style("top", (d3.event.pageY) + "px")
-            })
-          .on("mouseout", function() {		
-              div.transition()		
-                  .style("opacity", 0);	
-              });
-
-      
-    // increment x to move to next column
-    x = x + 1;
-    counter = counter + 1
-  }
-
-}
-
 function drawHueViz(){
-  var width = document.getElementById('chart2').clientHeight;
-  var height = document.getElementById('chart2').clientHeight * .9;
+
+  if (IsMobileCard()){ // if device is mobile
+    var width = document.getElementById('chart2').clientWidth;
+    var height = document.getElementById('chart2').clientWidth * .9;
+    var labelSize = "12px";
+    var barGap = 2.5;
+  }
+  else {
+    var width = document.getElementById('chart2').clientHeight;
+    var height = document.getElementById('chart2').clientHeight * .9;
+    var labelSize = "24px";
+    var barGap = 5;
+  }
+
+  d3.select("#chart2").style("height",height+10+"px")
 
   var huesvg = d3.select("#chart2").append("svg")
-  .attr("width", width)
-  .attr("height", height)
-
-  //groups = Object.keys(cardHues);
+    .attr("width", width)
+    .attr("height", height)
   
   groups = ["Any+Man", "Father", "Grandfather", "Brother", "Son",
               "Husband", "Grandson", "Any+Woman", "Boy", "Daughter", "Mother",
@@ -230,50 +99,51 @@ function drawHueViz(){
 
         huesvg.append("rect")
           .attr("x", z * (width / groups.length))
-          .attr("y", height - (i * (height / cardHues[group].length))) 
-          .attr("width", width / groups.length - 5)
+          .attr("y", (i * (height / cardHues[group].length)))
+          .attr("width", width / groups.length - barGap)
           .attr("height", height / cardHues[group].length * 1.5)
-          .style("fill", "hsl(" + cardHues[group][i][0] + ",50% ," + cardHues[group][i][1] +"%)");
-          
+          .style("fill", "hsl(" + cardHues[group][i][0] + ",50% , 50%)")
+
       }
   }
 
   for (z=0;z<groups.length;z++){
       drawOneGroupBar(groups[z])
-
-      huesvg.append('text')
-          .attr("x", z * (width / groups.length) + (width/groups.length/2))
-          .attr("y", height / 2)
-          .text(groups[z])
-          .style("font-size","8px")
-          .style("fill","white")
-          .attr('text-alignment','center')
-          .style("text-anchor", "middle")
-          //.attr("transform", "translate(" + z * (width / groups.length) + "," + height / 2 + ")")
-          //.attr("transform", "rotate(-90)")
   }
+
+  // Append Labels (THANK YOU https://observablehq.com/@weitinglin/d3-rotating-text-labels)
+  huesvg.selectAll('text.rotation')
+   .data(groups)
+   .enter()
+   .append('text')
+   .text((d)=>d.replace("+"," "))
+   .classed('rotation', true)
+   .attr('fill', 'black')
+   .attr('transform', (d,i)=>{
+       return 'translate( '+ (i* (width / groups.length)  + (width / groups.length / 4)) +' , '+height/2+'),'+ 'rotate(90)';})
+   .attr('x', 0)
+   .attr('y', 0)
+   .attr('text-alignment','center')
+   .style("text-anchor", "middle")
+   .style("font-size",labelSize)
+   .style('fill',"black")
+  
 }
 
-// change right selector to 'Boy'
-document.querySelector('#selectorRight').value="Boy"
 // create dictionary and fill with data using readData() function
 cardImages = {}
-readData() // this also preloads the page with the 'Girl vs Boy' category
+readData() // this also preloads the page with the 'Girl' category
 
 // for 2nd viz
 cardHues = {};
 readHueData();
 
-// if selector changes, clears svg1 and redraws cards with new category
-d3.select("#selectorLeft").on("change", function() {
-  svg1.selectAll("*").remove()
+// if selector changes, clears the gridvbox and redraws cards with new category
+d3.select("#selector").on("change", function() {
+  const parent = document.getElementById("galleryDiv");
+  while (parent.firstChild) {
+      parent.firstChild.remove();
+  }
   var newGroup = d3.select(this).property("value")
-  drawCardsLeft(newGroup)
-})
-
-// if selector changes, clears svg2 and redraws cards with new category
-d3.select("#selectorRight").on("change", function() {
-  svg2.selectAll("*").remove()
-  var newGroup = d3.select(this).property("value")
-  drawCardsRight(newGroup)
+  drawCards(newGroup)
 })
